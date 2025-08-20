4f1e977a57ac01812d8735bcb75dba76fa3522ba
“myuh250” <huyng2724@gmail.com>
2025-08-20 08:09:41 +0700

    Remove unnecessary constants

--- core/messages.py
+++ core/messages.py
@@ -1,12 +1,3 @@
-START_TEXT = "Xin chào! Cảm ơn bạn đã kết nối cùng chúng tôi."
-FORM_PROMPT = "Vui lòng nhấn vào link bên dưới để bổ sung thông tin:"
-BUTTON_FILLED = "Tôi đã điền form"
-THANK_YOU = "Cảm ơn bạn! Chúng tôi đã nhận được thông tin."
-
-WELCOME_MESSAGE = "Chào mừng bạn đến với bot của chúng tôi! Đây là lần đầu tiên bạn sử dụng dịch vụ."
-WELCOME_CTA = "Bắt đầu"
-AFTER_INTERACTION_MESSAGE = "Cảm ơn bạn đã tương tác! Chúng tôi rất vui được phục vụ bạn. Dưới đây là thông tin tiếp theo:"
-
 import requests 
 import json
 import time
